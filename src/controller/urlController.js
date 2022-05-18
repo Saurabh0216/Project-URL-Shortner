0cd169555054a904dbfa12f1fa46637dd1de82b3
Saurabh Tripathi <saurabhtripathitripathi1997@gmail.com>
2022-05-18 22:41:12 +0530

    prj4

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,6 +1,23 @@
 const shortid = require('shortid')
 const urlModel= require("../model/urlModel")
-//const isValid = require("../validator/validator")
+const redis = require("redis");
+const { promisify } = require("util");
+
+const redisClient = redis.createClient(
+    18639,
+    "redis-18639.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("0XHF40jfCCk9plgI7bgCt411B2HZFDf3", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+  });
+  const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+  const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 
 const isValid = (value) => {
     if (typeof value === 'undefined' || value === null) return false
@@ -8,8 +25,7 @@ const isValid = (value) => {
     if (typeof value === 'number' && value.toString().trim().length === 0) return false
     return true;
 }
-
-    const createUrl = async(req,res)=> 
+  const createUrl = async(req,res)=> 
  {
     try {
         
@@ -28,7 +44,7 @@ const isValid = (value) => {
         const isUrlPresent = await urlModel.findOne({longUrl:longUrl})
 
         if(isUrlPresent){
-            return res.status(400).send({status:true, message:"Short URL already created for this provide Long URL", data:isUrlPresent})
+            return res.status(200).send({status:true, message:"Short URL already created for this provide Long URL", data:isUrlPresent})
         }
 
 
@@ -36,11 +52,11 @@ const isValid = (value) => {
         const shortUrl = baseUrl+ '/' + urlCode
 
         const urlCreate = await urlModel.create({urlCode,longUrl,shortUrl})
+       
+       
         {
-            return res.status(201).send({status:true, message:"Short Url created successfully!",data:urlCreate})
+             return res.status(201).send({status:true, message:"Short Url created successfully!",data:urlCreate})
         }
-
-
     } catch (error) {
         return res.status(500).send({status:false, message: error.message})
         
@@ -53,22 +69,28 @@ const isValid = (value) => {
  {
      try {
 
-        let urlCode=req.params.urlCode
+        const urlCode=req.params.urlCode
+        
+        const checkUrlData = await GET_ASYNC(`${req.params.urlCode}`)
+     
+        const urlparse = JSON.parse(checkUrlData)
+      
+        if(checkUrlData) {
+           return  res.redirect(urlparse.longUrl)
+        }
 
         isUrlCodePresent = await urlModel.findOne({urlCode:urlCode})
-
-        // if(urlCode.trim().length==0){
-        //     return res.status(400).send({status:false, message:"Please provide urlcode"})
-        // }
-
+       
         if(!isUrlCodePresent){
             return res.status(404).send({status:false, message:"Url not found"})
         }
-        const data = await urlModel.findOne({ urlCode: urlCode }).select({longUrl:1, _id:0});
-        return res.status(201).send({status:true, data:data})
-
-         
-     } catch (error) {
+      
+   const urlData = req.params.urlCode
+    
+   await SET_ASYNC(`${urlData}`, JSON.stringify(isUrlCodePresent))
+            return res.redirect(isUrlCodePresent.longUrl)
+        
+            } catch (error) {
          return res.status(500).send({status:false, message:error.message})
          
      }
