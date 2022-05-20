c8e9bcf9971f4a4aa7ddc0180fa160cc0eea1127
Saurabh Tripathi <saurabhtripathitripathi1997@gmail.com>
2022-05-20 09:58:40 +0530

    prj4

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const urlController = require('../controller/urlController');
 
-router.post('/url/shorten', urlController.createUrl);
-router.get('/:urlCode',urlController.getCode)
+//url shorting
+router.post('/url/shorten', urlController.createUrl); //POST /url/shorten
+router.get('/:urlCode',urlController.getCode) //GET /:urlCode
 
 
 module.exports = router;
